6aff114e493f3d90c16aeddd2628e549c40e74d2
Sebastian Delmont <sd@notso.net>
2023-12-13 14:01:05 -0500

    More JSON cleanup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 export * from './lib/continents'
 
-let _entities
-if (typeof window === 'undefined')  {
-  import { createRequire } from "node:module"
-  const require = createRequire(import.meta.url)
-  _entities = require('./data/dxccByCode.json')
-} else {
-  import { _entities } from './data/dxccByCode.json'
-}
-
-export const ENTITIES = _entities
+export * from './data/dxccByCode.js'
 
